1e7776c8d91ce9bc8a1422d00f93a2a417b6d8c2
Ondrej Maksi <ondrej.maksi@gmail.com>
2020-09-26 09:12:15 +0200

    fixed 2/6, gitignore

--- cvicenie2/6.py
+++ cvicenie2/6.py
@@ -1,8 +1,6 @@
 n = int(input('zadaj n: '))
-for i in range(0, n-1):
-    if i == 0:
-        print(f'{(n-1)*" "}*{(n-1)*" "}')
-    if i == n - 2:
-        print(((2*n) - 1)*'*')
-    else:
-        print(f'{(n-i-2)*" "}*{(i)*"-"}-{(i)*"-"}*{(n-i-1)*" "}')
+
+print(f'{(n-1)*" "}*{(n-1)*" "}')
+for i in range(0, n-2):
+    print(f'{(n-i-2)*" "}*{(i)*"-"}-{(i)*"-"}*{(n-i-1)*" "}')
+print(((2*n) - 1)*'*')
